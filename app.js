f0c19455c3c7ab2d512b0806646ca75ff5bfa7ab
lcrowther-snyk <lawrence.crowther@snyk.io>
2022-05-23 14:15:25 +1000

    further removed libs to only show vulns for st

--- app.js
+++ app.js
@@ -3,22 +3,18 @@
  */
 
 var st = require('st');
-var dust = require('dustjs-linkedin');
-var dustHelpers = require('dustjs-helpers');
 var express = require('express');
 var http = require('http');
-var cons = require('consolidate');
 
 
 var app = express();
-var routes = require('./routes');
 
 // all environments
 app.set('port', process.env.PORT || 3001);
-app.engine('dust', cons.dust);
-cons.dust.helpers = dustHelpers;
 
-app.get('/', routes.index);
+app.get('/', function(request, response){
+  response.sendFile(process.cwd() +'/index.html');
+});
 
 // Static
 app.use(st({ path: './public', url: '/public' }));
